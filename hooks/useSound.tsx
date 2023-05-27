688fad54172deb7fd70750b86014e73c7bfc29e8
Arhun Saday <arhunsad@gmail.com>
2023-05-27 23:41:13 +0200

    Add useSound hook to play audio

---
+++ hooks/useSound.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import { Audio } from 'expo-av';
+
+export const useSound = (soundFile: any) => {
+  const [soundState, setSoundState] = useState<Audio.Sound | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (soundState) {
+        soundState.unloadAsync();
+      }
+    };
+  }, [soundState]);
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(soundFile);
+    setSoundState(sound);
+    await sound?.playAsync();
+  }
+
+  return playSound;
+};
